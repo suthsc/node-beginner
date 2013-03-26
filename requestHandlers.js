fbfe0c80411074e54e68e1c7d3e49e57e3f0f753
OpenShift guest <scott.sutherland@gmail.com>
2013-03-26 00:00:57 -0400

    Completed exercise.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
 
 function start(response) {
     console.log("Request handler 'start' was called.");
@@ -9,26 +11,58 @@ function start(response) {
         'charset=UTF-8" />' +
         '</head>' +
         '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<form action="/upload" enctype="multipart/form-data" method="post">' +
+        '<input type="file" name="upload" />' +
         '<input type="submit" value="Submit text" />' +
         '</form>' +
         '</body>' +
         '</html>';
         
-    exec("ls -lr", function (error, stdout, stderror) {
+    
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(response, request) {
+    console.log("Request handler 'upload' was called.");
+    var form = new formidable.IncomingForm();
+    console.log("About to parse.");
+    form.parse(request, function(error, fields, files) {
+        
+        console.log("parsing done");
+        /* possible error on Windows systems: tried to rename to an already
+         * existing file
+         */
+        fs.rename(files.upload.path, "/tmp/test.png", function (err) {
+            if (err) {
+                fs.unlink("/tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+        
         response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(body);
+        response.write("Received image: <br/>");
+        response.write("<img src='/show' />");
         response.end();
     });
 }
 
-function upload(response) {
-    console.log("Request handler 'upload was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello upload");
-    response.end();
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("/tmp/test.png", "binary", function (error, file) {
+        if (error) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    })
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var http = require('http');
-var url = require("url");
+var http = require('http'),
+    url = require("url");
 
 function start(route, handle) {
     
     function onRequest(request, response) {
+        
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
         
-        route(handle, pathname, response);
+        route(handle, pathname, response, request);
+        
     }
     
     http.createServer(onRequest).listen(process.env.PORT);
